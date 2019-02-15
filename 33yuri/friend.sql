a087036a2b71725e1052a774e07a01e166512e59
crane93 <youl9094@gmail.com>
2019-02-15 15:41:13 +0900

    2
    Former-commit-id: d3e240586a5e429602f8ac3e9099f3edcac15c73 [formerly 36b847265fab97e3e956736f13dd13fbb81ffa9b]
    Former-commit-id: d1029101aad68f3ce70943538f608e5cc17f7603

--- 33yuri/friend.sql
+++
@@ -1,9 +0,0 @@
-use java;
-
-create table friend(
-num int primary key auto_increment,		/*알아서 숫자가 1씩 증가한다*/
-name varchar(20) not null,
-jumin varchar(15) not null
-);
-desc friend;
-select * from friend;
